b4e6f5f435f93a9e2ff04484b4bbe789a97c2b32
Christopher <cj1mckay@gmail.com>
2020-02-01 15:54:46 +0000

    Add ability to run grab_book script remotely

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # import the Flask class from the flask module
 from flask import Flask, render_template, redirect, url_for, request
+import paramiko
 
 # create the application object
 app = Flask(__name__)
+ssh_client = None
 
 # use decorators to link the function to a url
 @app.route('/')
@@ -12,6 +14,18 @@ def home():
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')  # render a template
+
+@app.route('/control', methods=['GET', 'POST'])
+def control():
+    if(request.method == 'GET'):
+        return render_template('control.html')
+    if(request.method == 'POST'):
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(hostname='pichu',username='pi',password='turtlebot')
+        stdin,stdout,stderr=ssh_client.exec_command("cd catkin_ws; source devel/setup.bash; roscd sdp16_test/src; chmod +x *; rosrun sdp16_test pick_up_book.py")
+        return "stdin: " + str(stdin) + "\n" + "stdout: " + str(stdout) + "stderr" + str(stderr)
+
 
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
@@ -27,6 +41,3 @@ def login():
 # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-

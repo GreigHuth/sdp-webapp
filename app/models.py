c601b59f5d1c1732ab7e11d4d76ad294b62e442b
Greigashaska <greighuth@hotmail.co.uk>
2020-02-14 16:22:48 +0000

    added books schema in models.py

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 
 class User(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
@@ -11,3 +11,12 @@ class User(db.Model):
     #describes how to print the class
     def __repr__(self):
         return '<User {}>'.format(self.username) 
+
+class Book(db.Model):
+    bookId = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    author = db.Column(db.String(64))
+    holder = db.Column(db.Integer, db.ForeignKey('user.userId'))
+
+    def __repr__(self):
+        return '<Book {}>'.format(self.name)

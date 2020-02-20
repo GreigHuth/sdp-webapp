fdcac0d3e442083e6e5e9e78678a989b051aeb05
Greigashaska <greighuth@hotmail.co.uk>
2020-02-20 17:56:26 +0000

    home page added, lots of things

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,15 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 import paramiko
 
 #init app and config
 app = Flask(__name__)
 app.config.from_object(Config)
+
+#init flask-bootstrap
+bootstrap = Bootstrap(app)
 
 #init SSH client
 ssh_client = paramiko.SSHClient()
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app, db
+import paramiko
+from app import app, db, ssh_client
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
@@ -8,8 +9,10 @@ from app.forms import LoginForm, SignupForm
 
 @app.route('/')
 @app.route('/index')
-@login_required
+
 def index():
+  
+
     return render_template('index.html', title='Landing Page')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -17,14 +20,11 @@ def login():
 
     # dont need to log the user in if theyre already logged in
     if current_user.is_authenticated:
-        #TODO change this to redirect to /home
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
 
     form = LoginForm()
 
     #returns true if request method is POST, GET if false
-    # POST - Used when the page is submitting a form 
-    # GET - used when just returning html files
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
 
@@ -33,15 +33,15 @@ def login():
             flash('Invalid Username or password')
             return redirect(url_for('login'))
 
-        #this part actually does the user login
+        #actually does the user login
         login_user(user, remember=form.remember_me.data)
+        
 
-        next_page = request.args.get("next")
-
-        #this part returns the user to the page they tried to access if they tried to
+        #returns the user to the page they tried to access if they tried to
         #   access a page that requires authentication
+        next_page = request.args.get("next")
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index') #TODO this should redirect to /home
+            next_page = url_for('home') 
         return redirect(next_page) 
     
     return render_template('login.html', title ='Sign In', form=form)
@@ -54,12 +54,11 @@ def logout():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if current_user.is_authenticated:
-        return redirect(url_for('index')) #TODO this should redirect to /home
+        return redirect(url_for('home')) #TODO this should redirect to /home
 
     form = SignupForm()
+
     #returns true if request method is POST, GET if false
-    # POST - Used when the page is submitting a form 
-    # GET - used when just returning html files
     if form.validate_on_submit():    
         user = User(username=form.username.data)
         user.set_password(form.password.data)
@@ -68,3 +67,50 @@ def signup():
         flash('Successfully registered!')
         return redirect(url_for('login'))
     return render_template('signup.html', title = 'Sign up', form=form)
+
+#code for second demo remove before 3rd demo
+@app.route('/demo', methods=['GET','POST'])
+@login_required
+def demo2():
+
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname='pichu', username='pi', password='r00t')
+
+    if request.method == "POST":
+        if   'id_demo' in request.form:
+            stdin, stdout, stderr=ssh_client.exec_command('command/that/runs/book/id')
+            #TODO run book id script
+        
+        elif 'grab_demo' in request.form:
+            stdin, stdout, stderr=ssh_client.exec_command('command/that/runs/grabbing')
+            #TODO run book grabbing script
+
+        elif 'navi_demo' in request.form:
+            stdin, stdout, stderr=ssh_client.exec_command('command/that/runs/nav')
+            #TODO run navigation demo
+
+    return render_template('demo2.html', title = 'demo 2')
+
+
+@app.route('/home', methods=['GET', 'POST'])#might not need post
+@login_required
+def home(): 
+    return render_template('home.html', title = 'Home Page')
+
+#TODO all of this
+@app.route('/get', methods=['GET', 'POST'])
+@login_required
+def get():
+    return render_template('pickup.html', title = "Get Book")
+
+
+@app.route('/pickup', methods=['GET', 'POST'])
+@login_required
+def pickup():
+    return render_template('pickup.html', title = "Pick Up")
+
+
+@app.route('/reserve', methods=['GET', 'POST'])
+@login_required
+def reserve():
+    return render_template('reserve.html', title="Reserve")

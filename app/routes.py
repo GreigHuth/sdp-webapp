7bc9bcb8e99ab008c8ed7fe8db17f3f6cfbef65f
greighuth <greighuth@hotmail.co.uk>
2020-03-09 12:28:23 +0000

    started implementing search functionality

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,21 @@
 from flask import render_template, flash, redirect, url_for, request
 import paramiko
+import re
 from app import app, db, ssh_client
 from werkzeug.urls import url_parse
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Book
-from app.forms import LoginForm, SignupForm
+from app.forms import LoginForm, SignupForm, SearchForm
 
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-  
 
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     return render_template('index.html', title='Landing Page')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -46,10 +48,12 @@ def login():
     
     return render_template('login.html', title ='Sign In', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -60,48 +64,38 @@ def signup():
 
     #returns true if request method is POST, GET if false
     if form.validate_on_submit():    
+
         user = User(username=form.username.data)
         user.set_password(form.password.data)
         db.session.add(user) # ignore these errors
         db.session.commit()
         flash('Successfully registered!')
         return redirect(url_for('login'))
+
     return render_template('signup.html', title = 'Sign up', form=form)
 
-#code for second demo remove before 3rd demo
+
 @app.route('/get', methods=['GET','POST'])
 @login_required
 def get():
 
+    form = SearchForm()
 
-    #gets book names 
-    books = db.session.query(Book.title).all()
-    books = [book[0] for book in books]
+    if request.method == 'POST':  
 
-    if request.method == "POST":
-        #connect to BB with ssh, plan to use sockets or something better in the future
-        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(hostname='jigglypuff', username='pi', password='r00t')
+        #escapes non-aplphanumeric characters to prevent SQL injections
+        query = re.escape(form.search.data)
+        return redirect(url_for('get_search', query=query))
+   
+    return render_template('get.html', title = 'Get Book', form=form)
 
-        #container for demo script
-        demo_script = ""
-            
-        if 'id' in request.form:
-            
 
-            book_chosen = str(request.form["book"])
-            flash("Book Chosen - %s" % book_chosen)
-            #gets the label corresponding to the chosen book, its ugly, need to fix this
-            label_chosen = db.session.query(Book.label).filter(Book.title==book_chosen).first().label
-            demo_script = "rm $HOME/vision/BB-Book-Identification/scripts/label.txt; echo '%s' >> $HOME/vision/BB-Book-Identification/scripts/label.txt" % label_chosen
-            
-        stdin,stdout,stderr=ssh_client.exec_command(demo_script)
-
-        return render_template('get.html', title = 'Get Books', books=books)
-    
-    #if not POST then just return the page
-    else:
-        return render_template('get.html', title = 'Get Book', books=books)
+@app.route('/get/search/<query>', methods=['GET', 'POST'])
+@login_required
+def get_search(query):
+    flash('Searched for %s' % str(query))
+    #search the database for book titles 
+    return render_template('get_search.html', query=query)
 
 
 @app.route('/home', methods=['GET', 'POST'])#might not need post
